7f9539c0355bf610354475d22e46f2bb1cb013a2
sarp can <sarp95can@gmail.com>
2023-12-07 10:24:42 +0100

    formatting changes

--- apps/grid-helper/src/app/input/input.component.ts
+++ apps/grid-helper/src/app/input/input.component.ts
@@ -1,31 +1,36 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Units } from '../../../../../libs/fields/src/models/units';
 
 @Component({
-    selector: 'grid-helper-input',
-    standalone: true,
-    imports: [CommonModule, ReactiveFormsModule],
-    templateUrl: './input.component.html',
-    styleUrls: ['./input.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'grid-helper-input',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputComponent {
+  @Input() public form!: FormGroup;
+  @Input() public form_name = '';
+  @Input() public style_property = '';
+  @Input() public style_value = '';
+  @Input() public label = '';
+  @Input() public background_label = '';
+  @Input() public min_value: number | undefined = 0;
+  @Input() public orientation: 'horizontal' | 'vertical' = 'horizontal';
+  @Input() public removable = false;
+  @Input() public default_unit: Units = Units.FR;
+  @Input() public placeholder?: string;
 
-    @Input() public form!: FormGroup;
-    @Input() public form_name = '';
-    @Input() public style_property = '';
-    @Input() public style_value = '';
-    @Input() public label = '';
-    @Input() public background_label = '';
-    @Input() public min_value: number | undefined = 0;
-    @Input() public orientation: 'horizontal' | 'vertical' = 'horizontal';
-    @Input() public removable: boolean = false;
-    @Input() public default_unit: Units = Units.FR;
-    @Input() public placeholder?: string;
+  @Output() public remove = new EventEmitter<void>();
 
-    @Output() public remove = new EventEmitter<void>();
-
-    public units = Object.values(Units);
+  public units = Object.values(Units);
 }
